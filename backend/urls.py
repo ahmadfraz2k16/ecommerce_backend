42051ecefe7f6c9e2470d47786feef3df972e6d0
ahmadfraz2k16 <ahmadfraz2k16@gmail.com>
2023-02-20 17:09:12 +0500

    showed all products and single product from db
    changed the default path of images to be stored to backend/static/images
    added product model and installed pillow package to handle image upload
    serialized the model's data before showing to frontend, using rest-framework serializers
    finally all products and single product from db can be seen on frontend

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# image saving and preview related imports
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
 ]
+# setting path for saving images in custom dir static/images & preview images from static/images
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
+from base.models import Product
+from base.serializers import ProductSerializer
 from .products import products
 
 from rest_framework.decorators import api_view
@@ -27,14 +30,13 @@ def getRoutes(request):
 # for all products
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 # for single product
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
