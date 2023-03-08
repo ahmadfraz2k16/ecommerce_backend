a9f66beb45fa1e3ac6619ad970f67cc2cac3d783
ahmadfraz2k16 <ahmadfraz2k16@gmail.com>
2023-03-09 04:23:06 +0500

    used protected routes restriction for users
    created users route
    created users function getUsers()
    restricted normal users using decorator IsAdmin
    also used IsAuthenticated decorator for userprofile

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from base.models import Product
+from django.contrib.auth.models import User
 from base.serializers import ProductSerializer, UserSerializer, UserSerializerwithToken
-from .products import products
+# from .products import products
 
 # for customizing jwt token
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -22,9 +25,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 # changing default serializer behaviour, in base/url we will get this customized user data
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 # Create your views here.
 @api_view(['GET'])
@@ -47,9 +47,18 @@ def getRoutes(request):
 
 # for user profile
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+# for all users
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 # for all products
